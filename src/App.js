5208672b5511afea5d88e0fd47998704bdde52e2
zyx <casper445869@sina.com>
2022-11-12 10:06:34 +0800

    修改测试part1

--- src/App.js
+++ src/App.js
@@ -4,22 +4,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="shuzi.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          ZHUZHU <span className="heart">♥️</span>
         </p>
         <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          success
         </p>
       </header>
     </div>
